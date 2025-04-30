f05ccab047548c0b97f4cea64e612ed65ec6bc17
Edmont Nowitschichin <edmontnowitschichin@MacBook-Air.local>
2025-04-30 12:33:22 +0200

    speichern

---
+++ master_script.R
@@ -0,0 +1,11 @@
+# Initialize -------------------------------------------------------------------
+
+rm(list = ls())
+set.seed(1)
+setwd("~/Documents/R Projekte/business_project")
+
+# load required packages
+library(tidyverse)
+library(readxl)
+library(lubridate)
+
