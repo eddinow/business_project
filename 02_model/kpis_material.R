773f7638a517c21747fe153ac291e1b2de1f1b94
Kaspar Lewerenz <kasparlewerenz@air-von-kaspar.speedport.ip>
2025-05-21 15:18:58 +0200

    .

---
+++ 02_model/kpis_material.R
@@ -0,0 +1,17 @@
+# Initialisierung ----------------------------------------------------------------
+
+# rm(list = ls())
+# set.seed(1)
+
+library(tidyverse)
+library(tidyr)
+library(readxl)
+library(dplyr)
+library(ggplot2)
+library(MASS)
+library(shiny)
+library(DT)
+library(stringr)
+library(plotly)
+
+# Daten laden -------------------------------------------------------------------
